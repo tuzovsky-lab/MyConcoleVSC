6ea445b72860300a2640fa6efe80fe46a812401f
Тузовский Анатолий Федорович <63976413+tuzovsky-lab@users.noreply.github.com>
2023-04-20 10:42:23 +0300

    Улучшенная версия проекта

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Как вас зовут?");
+        string name = Console.ReadLine();
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine($"\n Привет {name}, сейчас {currentDate:d} at {currentDate:t}!");
+        Console.Write($"\n Нажмите любую клавишу для завершения...");
+        Console.ReadKey(true);
     }
 }
